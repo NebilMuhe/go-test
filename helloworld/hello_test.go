c820c9c566d85485639499f42a633c1ca56fa0d3
Nebil Muhidin <nebilmuhidin57@gmail.com>
2024-06-12 10:47:23 +0300

    add the test when empty parameter provided

--- helloworld/hello_test.go
+++ helloworld/hello_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Nebil")
-	want := "Hello, Nebil"
+	t.Run("say hello to people", func(t *testing.T) {
+		got := Hello("Nebil")
+		want := "Hello, Nebil"
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("say 'Hello, World' to empty name", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
 }
